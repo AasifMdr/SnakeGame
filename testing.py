ea8602705270ba246a9b2d62b9e53d40f3d663f3
Aasif Manandhar <aasifmanandhar09@gmail.com>
2021-10-04 18:14:11 +0545

    minor database changes

--- testing.py
+++ testing.py
@@ -1,17 +1,4 @@
 import pytest
-
-@pytest.fixture
-def tester():
-    username = "asd"
-    password = "asd"
-    return (username, password)
-
-
-def test1(tester):
-    username1 = "asd"
-    assert tester[0] == username1
-
-
-def test_2(tester):
-    password_check = "asd"
-    assert tester[1] == password_check
+@pytest.mark.parametrize("username , password",[("kill" , "kill"),("asd" , "asd")])
+def test_method(username , password):
+    assert username  == password
